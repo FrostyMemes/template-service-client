4c628320aa04cae32b9fe9b0714348759000f8a4
Anatoliy <lager11197@yandex.ru>
2023-02-07 20:11:42 +0300

    feat: added prototype of sending http request

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
-    console.log(process.env.REACT_APP_BACKEND_IP)
+    const apiUrl = `${process.env.REACT_APP_BACKEND_ADDRESS}/api`
+    const [markdown, setMarkdown] = useState('')
+    const [mdRender, setMdrender] = useState('')
+
+    const sendMarkdown = function (){
+        axios.get(`http://${apiUrl}?markdown=${markdown}`)
+            .then(response => setMdrender(response.data))
+            .catch(error => {
+                setMdrender(error.message)
+            });
+        //setMarkdown(`${apiUrl}?markdown=${markdown}`)
+    }
+
     return (
         <div>
             <div className="render-space">
                 <div className="input-area">
-                    <textarea placeholder={process.env.REACT_APP_BACKEND_IP}></textarea>
+                    <textarea placeholder={apiUrl}
+                              onChange={(e) => setMarkdown(e.target.value)}>
+                    </textarea>
                 </div>
                 <div className="render-area">
-                    Вывод
+                    {mdRender}
                 </div>
             </div>
-            <input type="button" value="Отправить"/>
+            <input type="button" value="Отправить" onClick={sendMarkdown}/>
         </div>
     );
 }
