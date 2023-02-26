15c9fe9fb5c225a1cf10b1ad26783b158f654d4a
FrostyMemes <lager11197@yandex.ru>
2023-02-26 23:16:46 +0300

    refactor: clear code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import EditorMarkdown from './components/editor/EditorMarkdown'
 
-
 function App() {
+
+
 
     return (
         <div>
-            <EditorMarkdown title="Example"/>
-            <input type="button" value="Save to DataBase"/>
+            <EditorMarkdown/>
         </div>
     );
 }
