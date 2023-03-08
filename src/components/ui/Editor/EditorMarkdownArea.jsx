b2403cf20669da73d62cd949e43d8314a0a6bbd7
FrostyMemes <lager11197@yandex.ru>
2023-03-09 00:06:37 +0300

    refactor: delete useless block

--- src/components/ui/Editor/EditorMarkdownArea.jsx
+++ src/components/ui/Editor/EditorMarkdownArea.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const EditorMarkdownArea = ({onChange, render}) => {
+const EditorMarkdownArea = ({onChange}) => {
 
     return (
-        <div className="editor-area">
-            <div className="input-area">
-                    <textarea placeholder={process.env.REACT_APP_BACKEND_ADDRESS}
-                              onChange={(e) => onChange(e.target.value)}>
-                    </textarea>
-            </div>
-            <div className="render-area" dangerouslySetInnerHTML={{__html: render}}></div>
+
+        <div className="input-area">
+            <textarea placeholder={process.env.REACT_APP_BACKEND_ADDRESS}
+                      onChange={(e) => onChange(e.target.value)}>
+            </textarea>
         </div>
+
     )
 }
 
