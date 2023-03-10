fa07cd5f3d7dafa7812d7d69352bdd1270fbc824
FrostyMemes <lager11197@yandex.ru>
2023-03-11 00:53:09 +0300

    refactor: changed style of function name

--- src/services/MarkdownService.js
+++ src/services/MarkdownService.js
@@ -2,7 +2,7 @@ import api from "../http";
 
 const serviceUrl = "markdown"
 
-export function SendMarkdown(markdown) {
+export function sendMarkdown(markdown) {
     return api.get(`${serviceUrl}`,{
         params: {
             markdown: markdown
