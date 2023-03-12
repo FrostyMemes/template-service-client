c79d01fb9857238b4b194a1fff7802ccb3dbef48
FrostyMemes <lager11197@yandex.ru>
2023-03-13 01:52:50 +0300

    feat: added template displaying

--- src/components/ui/Template/Template.jsx
+++ src/components/ui/Template/Template.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector} from "react-redux";
 import classes from "./Template.module.css";
 
@@ -8,7 +8,7 @@ const Template = () => {
 
     return (
         <div className={classes.template}>
-            {/*<div dangerouslySetInnerHTML={{__html: template.markup}}></div>*/}
+            {template && <div dangerouslySetInnerHTML={{__html: template.markup}}></div>}
         </div>
     );
 };
