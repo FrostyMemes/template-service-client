b4a91b895f4789d4102035b55c328dfda08bd7bc
FrostyMemes <lager11197@yandex.ru>
2023-03-16 23:24:19 +0300

    feat: added SCSS styles for some template tags

--- src/components/ui/Template/Template.jsx
+++ src/components/ui/Template/Template.jsx
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import {useSelector} from "react-redux";
-import classes from "./Template.module.css";
+import  "./Template.scss";
+
 
 const Template = () => {
 
     const template = useSelector(state => state.templates.currentTemplate)
 
     return (
-        <div className={classes.template}>
-            <div dangerouslySetInnerHTML={{__html: template?.markup}}></div>
+        <div className="template-block">
+                <div dangerouslySetInnerHTML={{__html: template?.markup}}></div>
         </div>
     );
 };
