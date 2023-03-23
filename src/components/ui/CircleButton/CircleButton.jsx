d51317bcd2dca9c853ee61a84025e521a922e624
FrostyMemes <lager11197@yandex.ru>
2023-03-24 00:42:05 +0300

    feat: created circle menu button component

---
+++ src/components/ui/CircleButton/CircleButton.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./CircleButton.scss";
+import "font-awesome/css/font-awesome.min.css"
+
+const CircleButton = () => {
+
+    const activateButton = () => {
+        document
+            .getElementById('circularMenu')
+            .classList.toggle('active');
+    }
+
+    return (
+        <div id="circularMenu" className="circular-menu">
+
+            <a className="circular-floating-btn" onClick={activateButton}>
+                <i className="fa fa-bars"></i>
+            </a>
+
+            <menu className="circular-items-wrapper">
+                <a href="#" className="fa fa-plus circular-menu-item"></a>
+                <a href="#" className="fa fa-pencil circular-menu-item"></a>
+                <a href="#" className="fa fa-trash circular-menu-item"></a>
+            </menu>
+
+        </div>
+
+
+    );
+};
+
+export default CircleButton;
