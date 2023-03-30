b4358256e813e64c467a7586dcc97286f2b1adcb
FrostyMemes <lager11197@yandex.ru>
2023-03-31 00:40:30 +0300

    delete: deleted useless component

--- src/components/ui/Editor/EditorRenderArea.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const EditorRenderArea = ({render}) => {
-    return (
-        <div className="render-area" dangerouslySetInnerHTML={{__html: render}}></div>
-    );
-};
-
-export default EditorRenderArea;
