47e9c661ea94196193b79337c35c34e3ee15ecfd
FrostyMemes <lager11197@yandex.ru>
2023-03-31 00:41:41 +0300

    feat: added first version of Editor page

--- src/components/pages/Editor/EditorPage.jsx
+++ src/components/pages/Editor/EditorPage.jsx
@@ -1,9 +1,65 @@
 import React from 'react';
+import {useEffect, useState} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import useDebounce from "../../hooks/use-debounce";
+import {sendMarkdown} from "../../../services/MarkdownService";
+import {fetchTemplates, saveTemplate, deleteTemplate} from "../../../store/reducers/TemplateActions";
+import MarkdownArea from "../../ui/MarkdownArea/MarkdownArea";
+import Template from "../../ui/Template/Template";
+
 
 const EditorPage = () => {
+
+    // const templates = useSelector(state => state.templates.templates)
+    // const dispatch = useDispatch()
+
+    const [mdMarkdown, setMarkdown] = useState('mTest')
+    const [mdRender, setRender] = useState('rTest')
+    const [title, setTitle] = useState('Test')
+
+    const debouncedRenderTerm = useDebounce(mdMarkdown, 700);
+
+    const renderTemplate = (markdown) => {
+        sendMarkdown(markdown)
+            .then(response => setRender(response.data))
+            .catch(error => setRender(error.message))
+    }
+
+/*    useEffect(() => {
+        if (debouncedSearchTerm) {
+            renderTemplate(mdMarkdown)
+        } else {
+            setRender('');
+        }
+    }, [])*/
+
+    useEffect(() => {
+        if (debouncedRenderTerm) {
+            renderTemplate(mdMarkdown)
+        } else {
+            setRender('');
+        }
+    }, [debouncedRenderTerm])
+
     return (
-        <div>
-            
+        <div className="editor">
+            <div className="template_title">
+                <h1>{title}</h1>
+            </div>
+
+            <div className="control_buttons">
+                <input type="button" value="Сохранить"/>
+            </div>
+
+            <div className="editor_area">
+                <div className="markdown_area">
+                    <MarkdownArea onChange={markdown => setMarkdown(markdown)}/>
+                </div>
+
+                <div className="render_area">
+                    <Template markup={mdRender}/>>
+                </div>
+            </div>
         </div>
     );
 };
