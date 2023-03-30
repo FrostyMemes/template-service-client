5959b98c98cc23b70fcdc15cddeb00757b3959c3
FrostyMemes <lager11197@yandex.ru>
2023-03-30 19:36:20 +0300

    refactor: changed props for TemplateItem

--- src/components/ui/TemplateList/TemplateList.jsx
+++ src/components/ui/TemplateList/TemplateList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import TemplateItem from "./TemplateItem";
 import classes from "./TemplateList.module.css"
+import {Link} from "react-router-dom";
 
 const TemplateList = (props) => {
     return (
         <div className={classes.templateList}>
             {props.templateList.map(template =>
-                <TemplateItem template={template} key={template.id}/>
+                <TemplateItem
+                    key={template.id}
+                    id={template.id}
+                    title={template.title}
+                />
             )}
         </div>
     );
