72e002a480e84436012af3b15fdb5b0f9c3f7780
FrostyMemes <lager11197@yandex.ru>
2023-03-31 00:40:57 +0300

    refactor: renamed markdown editor area component

--- src/components/ui/MarkdownArea/MarkdownArea.jsx
+++ src/components/ui/MarkdownArea/MarkdownArea.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import classes from "./Editor.module.css";
+import classes from "./MarkdownArea.module.css";
 
-const EditorMarkdownArea = ({onChange}) => {
+const MarkdownArea = ({onChange}) => {
 
     return (
 
-        <div className={classes.editorMarkdownArea}>
+        <div className={classes.MarkdownAreaBlock}>
 {/*            <textarea placeholder={process.env.REACT_APP_BACKEND_ADDRESS}
                       onChange={(e) => onChange(e.target.value)}>
             </textarea>*/}
-
         </div>
 
     )
 }
 
-export default EditorMarkdownArea
+export default MarkdownArea
