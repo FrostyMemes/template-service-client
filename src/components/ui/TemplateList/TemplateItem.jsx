a55cf7e760d8332d123ae9acc06f342d27ceef83
FrostyMemes <lager11197@yandex.ru>
2023-03-30 19:35:51 +0300

    feat: added links for Template Items

--- src/components/ui/TemplateList/TemplateItem.jsx
+++ src/components/ui/TemplateList/TemplateItem.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import classes from "./TemplateList.module.css";
 
 
-const TemplateItem = ({template}) => {
+const TemplateItem = ({id, title}) => {
     return (
-        <div className={classes.templateItem}>
-            <div className={classes.templateContent}>
-                {template.title}
-            </div>
-        </div>
+        <>
+            {/*<Link to={`/${id}`}>*/}
+                <div className={classes.templateItem}>
+                    <div className={classes.templateContent}>
+                        {title}
+                    </div>
+                </div>
+            {/*</Link>*/}
+        </>
     );
 };
 
