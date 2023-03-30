03e85721c99f0dede0f68ec4ca4f7fb1878b06a5
FrostyMemes <lager11197@yandex.ru>
2023-03-30 19:28:50 +0300

    feat: added routing components

--- src/components/ui/CircleButton/CircleButton.jsx
+++ src/components/ui/CircleButton/CircleButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./CircleButton.scss";
+import {Routes, Route, Link} from "react-router-dom";
 import "font-awesome/css/font-awesome.min.css"
 
 const CircleButton = () => {
@@ -12,20 +13,15 @@ const CircleButton = () => {
 
     return (
         <div id="circularMenu" className="circular-menu">
-
             <a className="circular-floating-btn" onClick={activateButton}>
                 <i className="fa fa-bars"></i>
             </a>
-
             <menu className="circular-items-wrapper">
                 <a href="#" className="fa fa-plus circular-menu-item"></a>
                 <a href="#" className="fa fa-pencil circular-menu-item"></a>
                 <a href="#" className="fa fa-trash circular-menu-item"></a>
             </menu>
-
         </div>
-
-
     );
 };
 
