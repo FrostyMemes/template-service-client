b6cbd649a1952dce503a8e87beef76b5c3cd87a4
FrostyMemes <lager11197@yandex.ru>
2023-04-13 01:00:14 +0300

    fix: deleted useless import

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit";
 import templateReducer from "./reducers/TemplateSlice"
-import editorReducer from "./reducers/EditorSlice";
 
 export default configureStore({
     reducer: {
