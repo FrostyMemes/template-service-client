c092cac039c03ee553eca8c3359d20dbf5aeae7d
FrostyMemes <lager11197@yandex.ru>
2023-04-13 00:46:47 +0300

    refactor: clean code

--- src/components/ui/TemplateList/TemplateList.jsx
+++ src/components/ui/TemplateList/TemplateList.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import TemplateItem from "./TemplateItem";
 import classes from "./TemplateList.module.css"
 import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 
 const TemplateList = (props) => {
+    const dispatch = useDispatch()
+
     return (
         <div className={classes.templateList}>
             {props.templateList.map(template =>
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
 import templateReducer from "./reducers/TemplateSlice"
+import editorReducer from "./reducers/EditorSlice";
 
 export default configureStore({
     reducer: {
-        templates: templateReducer
+        templates: templateReducer,
     },
 })
