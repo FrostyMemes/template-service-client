e0963d890deea408f7d3bba6f8917df582255119
FrostyMemes <lager11197@yandex.ru>
2023-04-13 00:38:30 +0300

    feat: added routing to create and update template page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import TemplateList from "./components/ui/TemplateList/TemplateList";
 import Template from "./components/ui/Template/Template";
 import {fetchTemplates} from "./store/reducers/TemplateActions";
 import {Route, Routes} from "react-router-dom";
-import EditorPage from "./components/pages/Editor/EditorPage";
+import Editor from "./components/pages/Editor/Editor";
 import TemplaterPage from "./components/pages/Templater/TemplaterPage";
+import EditorNewPage from "./components/pages/Editor/EditorNewPage";
+import EditorUpdatePage from "./components/pages/Editor/EditorUpdatePage";
 
 function App() {
     return (
         <>
-           <TemplaterPage/>
-            {/*<EditorPage/>*/}
+           {/*<TemplaterPage/>*/}
+            {/*<Editor/>*/}
             <Routes>
-                <Route path="/:id" element={<TemplaterPage/>}/>
-                <Route path="/Editor" element={<EditorPage/>}/>
+                <Route path="/" element={<TemplaterPage/>}/>
+                <Route path="/Editor/NewTemplate" element={<EditorNewPage/>}/>
+                <Route path="/Editor/:id" element={<EditorUpdatePage/>}/>
             </Routes>
         </>
     );
