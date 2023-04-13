5d585cbbe4a3e3269c227e273146e35625d9b12d
FrostyMemes <lager11197@yandex.ru>
2023-04-14 01:21:33 +0300

    feat: added links to new pages

--- src/components/ui/CircleButton/CircleButton.jsx
+++ src/components/ui/CircleButton/CircleButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 import "./CircleButton.scss";
-import {Routes, Route, Link} from "react-router-dom";
 import "font-awesome/css/font-awesome.min.css"
 
 const CircleButton = () => {
 
-    const activateButton = () => {
+    const openMenu = () => {
         document
             .getElementById('circularMenu')
             .classList.toggle('active');
@@ -13,12 +13,12 @@ const CircleButton = () => {
 
     return (
         <div id="circularMenu" className="circular-menu">
-            <a className="circular-floating-btn" onClick={activateButton}>
+            <a className="circular-floating-btn" onClick={openMenu}>
                 <i className="fa fa-bars"></i>
             </a>
             <menu className="circular-items-wrapper">
-                <a href="#" className="fa fa-plus circular-menu-item"></a>
-                <a href="#" className="fa fa-pencil circular-menu-item"></a>
+                <NavLink to={"/editor/new"} className="fa fa-plus circular-menu-item"></NavLink>
+                <NavLink to={"/editor/:id"} className="fa fa-pencil circular-menu-item"></NavLink>
                 <a href="#" className="fa fa-trash circular-menu-item"></a>
             </menu>
         </div>
