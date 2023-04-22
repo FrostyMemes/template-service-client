bc7fe160364566040212d15f79cc55e4c4be5600
FrostyMemes <lager11197@yandex.ru>
2023-04-23 02:17:01 +0300

    feat: added condition for showing of control buttons

--- src/components/ui/CircleButton/CircleButton.jsx
+++ src/components/ui/CircleButton/CircleButton.jsx
@@ -3,23 +3,28 @@ import {NavLink} from "react-router-dom";
 import "./CircleButton.scss";
 import "font-awesome/css/font-awesome.min.css"
 
-const CircleButton = () => {
+const CircleButton = ({handlerDeleteTemplateFunction, currentTemplateId}) => {
 
+    const menuId = 'circularMenu'
     const openMenu = () => {
         document
-            .getElementById('circularMenu')
+            .getElementById(menuId)
             .classList.toggle('active');
     }
 
     return (
-        <div id="circularMenu" className="circular-menu">
+        <div id={menuId} className="circular-menu">
             <a className="circular-floating-btn" onClick={openMenu}>
                 <i className="fa fa-bars"></i>
             </a>
             <menu className="circular-items-wrapper">
-                <NavLink to={"/editor/new"} className="fa fa-plus circular-menu-item"></NavLink>
-                <NavLink to={"/editor/:id"} className="fa fa-pencil circular-menu-item"></NavLink>
-                <a href="#" className="fa fa-trash circular-menu-item"></a>
+                    <NavLink to={"/editor/new"} className="fa fa-plus circular-menu-item"/>
+
+                {currentTemplateId &&
+                    <NavLink to={`/editor/${currentTemplateId}`} className="fa fa-pencil circular-menu-item"/>}
+
+                {currentTemplateId &&
+                    <a onClick={handlerDeleteTemplateFunction} className="fa fa-trash circular-menu-item"></a>}
             </menu>
         </div>
     );
