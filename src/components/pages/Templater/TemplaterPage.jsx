dbc8e54e9d277686483e69cfe3fe793fd4b2c0a0
FrostyMemes <lager11197@yandex.ru>
2023-04-23 02:20:37 +0300

    feat: showing of template at main page

--- src/components/pages/Templater/TemplaterPage.jsx
+++ src/components/pages/Templater/TemplaterPage.jsx
@@ -2,31 +2,55 @@ import React, {useEffect, useState} from 'react';
 import classes from "./TemplaterPage.module.scss";
 import TemplateList from "../../ui/TemplateList/TemplateList";
 import Template from "../../ui/Template/Template";
-import {fetchTemplates} from "../../../store/reducers/TemplatesActions";
+import {deleteTemplate, fetchTemplates} from "../../../store/reducers/TemplatesActions";
 import {useDispatch, useSelector} from "react-redux";
 import CircleButton from "../../ui/CircleButton/CircleButton";
+import {getTemplateById} from "../../../services/TemplateService";
 
 const TemplaterPage = () => {
 
     const templates = useSelector(state => state.templates.templates)
     const dispatch = useDispatch()
 
-    const [currentId, setCurrentId] = useState(null)
+    const [currentTemplateId, setCurrentTemplateId] = useState(null)
     const [markup, setMarkup] = useState(null)
+
+    const handlerDeleteTemplate = () => {
+        if (currentTemplateId){
+            dispatch(deleteTemplate(currentTemplateId))
+            setCurrentTemplateId(null)
+        }
+    }
 
     useEffect(() => {
         dispatch(fetchTemplates())
     }, [])
 
+    useEffect(() => {
+        if (currentTemplateId)
+            getTemplateById(currentTemplateId)
+                .then(response => {
+                    setMarkup(response.data.markup)
+                })
+                .catch(error => setMarkup(error))
+    },[currentTemplateId])
+
+
     return (
         <div className={classes.TemplaterPage}>
             <div className={classes.TemplateListArea}>
-                <TemplateList templateList={templates}/>
+                <TemplateList
+                    templateList={templates}
+                    changeCurrentIdTemplate={setCurrentTemplateId}
+                />
             </div>
             <div className={classes.TemplateArea}>
-                <Template/>
+                <Template markup={markup}/>
             </div>
-            <CircleButton/>
+            <CircleButton
+                currentTemplateId={currentTemplateId}
+                handlerDeleteTemplateFunction={handlerDeleteTemplate}
+            />
         </div>
     );
 };
