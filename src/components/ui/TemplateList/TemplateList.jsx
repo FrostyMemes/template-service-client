06744e3d19a116af6860127c5f37784346586e2c
FrostyMemes <lager11197@yandex.ru>
2023-04-25 01:29:11 +0300

    feat: added switch panel between templates and word files

--- src/components/ui/TemplateList/TemplateList.jsx
+++ src/components/ui/TemplateList/TemplateList.jsx
@@ -3,12 +3,15 @@ import TemplateItem from "./TemplateItem";
 import classes from "./TemplateList.module.css"
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
+import SwitchPanelButton from "../SwitchPlate/SwitchPanelButton";
 
 const TemplateList = ({templateList, changeCurrentIdTemplate}) => {
     const dispatch = useDispatch()
 
     return (
         <div className={classes.templateList}>
+            <SwitchPanelButton faIconClassStyle="fa fa-file-word-o "/>
+            <SwitchPanelButton faIconClassStyle="fa fa-file-word-o "/>
             {templateList.map(template =>
                 <TemplateItem
                     key={template.id}
