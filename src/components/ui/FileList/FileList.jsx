7a86a85ec53ab1a46ef83e2406955476d5619886
FrostyMemes <lager11197@yandex.ru>
2023-04-29 00:48:23 +0300

    feat: created components for showing user files

---
+++ src/components/ui/FileList/FileList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "./FileList.module.scss";
+
+const FileList = () => {
+    return (
+        <div className={classes.fileList}>
+
+        </div>
+    );
+};
+
+export default FileList;
---
+++ src/components/ui/FileList/FileListItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FileListItem = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FileListItem;
